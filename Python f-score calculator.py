7e4b234ed9743df1e18e57ba05c94d445826f3e4
Ting Guan Heng <tguanheng@outlook.sg>
2022-06-28 22:42:20 +0800

    Update Python f-score calculator.py
    I managed to do away with the YahooFinance package and built my own API using codes adapted from another user.

--- Python f-score calculator.py
+++ Python f-score calculator.py
@@ -1,77 +1,101 @@
-''' Python Piotrotski f-score calculator'''
 '''
-Note:
-I've checked the table values of YahooFinancials module with
-Yahoo Finance website itself and found out that many values
-are wrong. 
-
-This added with the fact that webscraping with Python is far
-beyond my skill level as of now means the webscrape codes
-should be taken with a pinch of salt.
-
-This CANNOT be used as an actual product but should be viewed 
-as a display of Python data manipulation skillset.
-
-Check out my R script for the usable product that I actually use
+My own attempt at creating Yahoo Finance API by adapting the codes from StackOverflow by 'QHarr'
+https://stackoverflow.com/questions/58315274/r-web-scraping-yahoo-finance-after-2019-change
+Adapting code because Python code provided does not work
 '''
 
+import requests as re
+from bs4 import BeautifulSoup as bs
 import pandas as pd
-import json
-import numpy as np
-from yahoofinancials import YahooFinancials
 
-ticker = 'AAPL'
-rawdata = YahooFinancials(ticker)
+import os
 
-'''Income Statement'''
+os.chdir('D:/Random projects/Stock Market')
 
-income_Statement_data_qt = rawdata.get_financial_stmts('annual', 'income')
-latest_is = pd.DataFrame(income_Statement_data_qt['incomeStatementHistory'][ticker][0])
-last_year_is = pd.DataFrame(income_Statement_data_qt['incomeStatementHistory'][ticker][1])
-two_years_ago_is = pd.DataFrame(income_Statement_data_qt['incomeStatementHistory'][ticker][2])
-income_statement = pd.concat([latest_is,last_year_is,two_years_ago_is], axis = 1).transpose()
-income_statement['netIncome_is'] = income_statement['netIncome']
-income_statement = income_statement.drop(columns = 'netIncome')
+Ticker = 'AAPL'
+company = Ticker.upper()
 
-'''Balance Sheet'''
+## Vital clue to fixing the problem, some form of authentication is required
+header = {[insert own header]}
+###############Income Statement################
+income_statement = re.get('https://finance.yahoo.com/quote/'+company+'/financials?p='+company, headers=header)
+soup = bs(income_statement.content, features = 'html.parser')
 
-balance_Sheet_data_qt = rawdata.get_financial_stmts('annual','balance')
-latest_bs = pd.DataFrame(balance_Sheet_data_qt['balanceSheetHistory'][ticker][0])
-last_year_bs = pd.DataFrame(balance_Sheet_data_qt['balanceSheetHistory'][ticker][1])
-two_years_ago_bs = pd.DataFrame(balance_Sheet_data_qt['balanceSheetHistory'][ticker][2])
-bs = pd.concat([latest_bs,last_year_bs,two_years_ago_bs], axis = 1).transpose()
+income_statement_results = []
+## Within inspect, there is an element covering the entire table row that has "fi-row", use .fi-row
+for row in soup.select('.fi-row'):
+## Within the subelement, there are cells that can be identified with "title" (for breakdown) and "data-test=fin-col" (for values)
+## .text only pulls out actual values in <span>
+    income_statement_results.append([i.text for i in row.select('[title],[data-test="fin-col"]')])
 
-'''Statement of Cash Flow'''
+income_statement_results = pd.DataFrame(income_statement_results)
+for col in income_statement_results.columns[1:]:
+    income_statement_results[col] = income_statement_results[col].apply(lambda x: float(x.replace(',','').replace('-','0')))
 
-cash_flow_data_qt = rawdata.get_financial_stmts('annual','cash')
-latest_cf = pd.DataFrame(cash_flow_data_qt['cashflowStatementHistory'][ticker][0])
-last_year_cf = pd.DataFrame(cash_flow_data_qt['cashflowStatementHistory'][ticker][1])
-two_years_ago_cf = pd.DataFrame(cash_flow_data_qt['cashflowStatementHistory'][ticker][2])
-cf = pd.concat([latest_cf,last_year_cf,two_years_ago_cf], axis = 1).transpose()
-cf['netIncome_cf'] = cf['netIncome']
-cf = cf.drop(columns = ['netIncome'])
+## No common identification in header
+## At best use "BdB" to pull out dates, "Breakdown" must be hardcoded in
+title = [i.text for i in soup.select('.BdB')][0:5]
+title = pd.DataFrame([title])
 
-df = pd.concat([income_statement,bs,cf], axis = 1)
-df['positive_net_income'] = np.where(df['netIncome_is'] > 0, 1, 0)
-df['positive_roa'] = np.where((df['netIncome_is']/df['totalAssets']) > 0, 1, 0)
-df['positive_ocf'] = np.where(df['totalCashFromOperatingActivities'] > 0, 1, 0)
-df['ocf_more_than_net_income'] = np.where((df['totalCashFromOperatingActivities'] > df['netIncome_is']), 1, 0)
-df['long_term_debt_ratio_improvement'] = np.where((df['longTermDebt']/df['totalAssets']) < (df['longTermDebt'].shift(-1)/df['totalAssets'].shift(-1)), 1, 0)
-df['current_ratio_improvement'] = np.where(((df['totalCurrentAssets']/df['totalCurrentLiabilities']) > df['totalCurrentAssets'].shift(-1)/df['totalCurrentLiabilities'].shift(-1)), 1, 0)
-df['shareissuance'] = np.where(df['issuanceOfStock'] > 0, 0, 1)
-df['gross_margin_improvement'] = np.where(((df['grossProfit']/df['totalRevenue']) > (df['grossProfit'].shift(-1))/df['totalRevenue'].shift(-1)),1,0)
-df['asset_turnover_improvement'] = np.where((df['totalRevenue']/((df['totalAssets']+df['totalAssets'].shift(-1))/2)) > (df['totalRevenue'].shift(-1)/((df['totalAssets'].shift(-1)+df['totalAssets'].shift(-2))/2)),1,0)
-f_score = df[['positive_net_income',
-                        'positive_roa',
-                        'positive_ocf',
-                        'ocf_more_than_net_income',
-                        'long_term_debt_ratio_improvement',
-                        'current_ratio_improvement',
-                        'shareissuance',
-                        'gross_margin_improvement',
-                        'asset_turnover_improvement'
-    ]].head(1)
-f_score['score'] = f_score.sum(axis = 1)
-f_score['comment'] = np.where(f_score['score'] >= 8, 'Good Investment', 'Bad Investment')
-front_labels = ['comment','score']
-f_score = f_score[front_labels + [col for col in f_score.columns if col not in front_labels]]
+title_row = pd.concat([pd.DataFrame(['Breakdown']),title],axis = 1)
+title_row.columns = range(0,6)
+
+Income_statement_table = pd.concat([title_row,income_statement_results], axis = 0, ignore_index = True)
+Income_statement_table = Income_statement_table.transpose()
+Income_statement_table.columns = Income_statement_table.iloc[0,:]
+Income_statement_table = Income_statement_table[1:]
+
+
+###############Balance Sheet################
+balance_sheet = re.get('https://finance.yahoo.com/quote/'+company+'/balance-sheet?p='+company, headers=header)
+soup = bs(balance_sheet.content, features = 'html.parser')
+
+balance_sheet_results = []
+for row in soup.select('.fi-row'):
+        balance_sheet_results.append([i.text for i in row.select('[title],[data-test="fin-col"]')])
+
+balance_sheet_results = pd.DataFrame(balance_sheet_results)
+for col in balance_sheet_results.columns[1:]:
+    balance_sheet_results[col] = balance_sheet_results[col].apply(lambda x: float(x.replace(',','').replace('-','0')))
+
+
+title = [i.text for i in soup.select('.BdB')][0:5]
+title = pd.DataFrame([title])
+
+title_row = pd.concat([pd.DataFrame(['Breakdown']),title],axis = 1)
+title_row.columns = range(0,6)
+
+balance_sheet_table = pd.concat([title_row,balance_sheet_results], axis = 0, ignore_index = True)
+balance_sheet_table = balance_sheet_table.transpose()
+balance_sheet_table.columns = balance_sheet_table.iloc[0,:]
+balance_sheet_table = balance_sheet_table[1:]
+
+###############Cash Flow Statement################
+cash_flow = re.get('https://finance.yahoo.com/quote/'+company+'/cash-flow?p='+company, headers=header)
+soup = bs(cash_flow.content, features = 'html.parser')
+
+cash_flow_results = []
+for row in soup.select('.fi-row'):
+        balance_sheet_results.append([i.text for i in row.select('[title],[data-test="fin-col"]')])
+
+cash_flow_results = pd.DataFrame(cash_flow_results)
+for col in cash_flow_results.columns[1:]:
+    cash_flow_results[col] = cash_flow_results[col].apply(lambda x: float(x.replace(',','').replace('-','0')))
+
+
+title = [i.text for i in soup.select('.BdB')][0:5]
+title = pd.DataFrame([title])
+
+title_row = pd.concat([pd.DataFrame(['Breakdown']),title],axis = 1)
+title_row.columns = range(0,6)
+
+cash_flow_table = pd.concat([title_row,cash_flow_results], axis = 0, ignore_index = True)
+cash_flow_table = cash_flow_table.transpose()
+cash_flow_table.columns = cash_flow_table.iloc[0,:]
+cash_flow_table = cash_flow_table[1:]
+
+################Full table###############
+full_table = Income_statement_table.merge(
+    balance_sheet_table, how = 'left', on = 'Breakdown').merge(
+        cash_flow_table, how = 'left', on = 'Breakdown')
+
